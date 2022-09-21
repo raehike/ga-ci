05b2c8f08601a201f00c41cebc261a29ce433ba9
Benj Trees <thefirstmuffinman+ghalt@gmail.com>
2022-09-21 07:02:33 +0100

    small tweaks all over to prep for talk

--- binary-search-typed.py
+++ binary-search-typed.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
-
 import math
 from typing import Optional
-
 
 def binary_search(l: list[int], x: int) -> Optional[int]:
     return binary_search_inner(l, x, 0, len(l) - 1)
 
-
-# TODO bad stuff going on with floor and idx_left lol. the indexing is coupled
-def binary_search_inner(
-    l: list[int], x: int, idx_left: int, idx_right: int
-) -> Optional[int]:
+# take care for the awkward indexing (floor and idx_left are coupled in a surprising way)
+def binary_search_inner(l: list[int], x: int, idx_left: int, idx_right: int) -> Optional[int]:
     if idx_left == idx_right:
         if x == l[idx_left]:
             return idx_left
@@ -20,11 +15,12 @@ def binary_search_inner(
     else:
         idx = math.floor((idx_left + idx_right) / 2)
         if x < l[idx]:
-            return binary_search_inner(l, x, idx_left, idx - 1)
+            return binary_search_inner(l, x, idx_left, idx-1)
         elif x > l[idx]:
-            return binary_search_inner(l, x, idx + 1, idx_right)
-        else:  # must be equal
+            return binary_search_inner(l, x, idx+1, idx_right)
+        else: # must be equal
             return idx
+
 
 
 print(binary_search([0, 1, 2, 3, 4, 5, 6], 0))
--- binary-search.py
+++ binary-search.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-
 import math
 from typing import Optional
 
 def binary_search(l, x):
-    return binary_search_inner(l, x, 0, len(l)-1)
+    return binary_search_inner(l, x, 0, len(l) - 1)
 
-# TODO bad stuff going on with floor and idx_left lol. the indexing is coupled
+# take care for the awkward indexing (floor and idx_left are coupled in a surprising way)
 def binary_search_inner(l, x, idx_left, idx_right):
     if idx_left == idx_right:
         if x == l[idx_left]:
@@ -22,4 +21,6 @@ def binary_search_inner(l, x, idx_left, idx_right):
         else: # must be equal
             return idx
 
-print(binary_search([0,1,2,3,4,5,6], 0))
+
+
+print(binary_search([0, 1, 2, 3, 4, 5, 6], 0))
